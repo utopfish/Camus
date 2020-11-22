bbb435b3f0ed83abd0a117dd8b1db44f0d34a654
liumeng <utopfish@163.com>
2020-11-22 22:44:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# camus
+
+> a simple bidirectional-link markdown editor
+
+## 安装依赖
+
+```
+npm install
+```
+
+### 本地启动项目
+
+```
+npm run electron:serve
+```
+
+### 构建安装包
+
+```
+npm run electron:build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+## 长远目标
+1. 完成双向链接。
+2. 显示知识谱图
+
+## 中期目标
+1. 加入双窗口
+1. 加入typora一个所见即所得，使用veditor
+
+## 短期目标
+完成TODOlist
---
+++ src/components/FileManager/index.vue
@@ -0,0 +1,58 @@
+<!--
+ * @Description: 左侧文件搜索组件
+ * @Author: sufen
+ * @Date: 2020-05-20 16:08:49
+ * @LastEditTime: 2020-06-14 10:44:55
+ * @LastEditors: sufen
+ -->
+<template>
+  <div class="search-container">
+    <!-- <el-input placeholder="请输入内容" v-bind="$attrs" v-on="$listeners">
+      <el-button slot="append" icon="el-icon-search" @click.native="onSearch()" />
+    </el-input> -->
+
+    <el-button  icon="el-icon-document-add" @click.native="createFile()" type="primary" ></el-button>
+    <!-- <el-dropdown-item divided @click.native="importFile()">导入文件</el-dropdown-item> -->
+
+  </div>
+</template>
+
+<script>
+// TODO:1. 加入文件夹功能
+// TODO:2. 加入文件拖拽功能
+export default {
+  name: 'FileManager',
+  methods: {
+    // 搜索笔记
+    onSearch() {
+      this.$emit('search')
+    },
+    // 新建笔记
+    createFile() {
+      this.$emit('create')
+    },
+    // 导入文件
+    importFile() {
+      this.$emit('import')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.search-container {
+  display: flex;
+  align-items: center;
+  padding: 12px 10px;
+  background: #daecfe;
+
+  .el-dropdown {
+    .el-button--small {
+      margin-left: 10px;
+      padding: 6px;
+      font-size: 14px;
+      border-radius: 30%;
+    }
+  }
+}
+</style>
